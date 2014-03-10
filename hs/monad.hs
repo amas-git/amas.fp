f654f1bd6e30d6f249be555bc3aa87ab3a0a5110
amas <zhoujb.cn@gmail.com>
2014-03-11 00:30:56 +0800

    understand monads

---
+++ hs/monad.hs
@@ -0,0 +1,50 @@
+
+getS1 "S0" = Just "S1"
+getS1 _ = Nothing
+
+
+getS2 "S1" = Just "S2"
+getS2 _ = Nothing
+
+
+getS3 "S2" = Just "S3"
+getS3 _ = Nothing
+
+
+doSomething m = case (getS1 m) of
+  Nothing -> Nothing
+  Just m  -> case (getS2 m) of
+    Nothing -> Nothing
+    Just m  -> case (getS3 m) of
+      Nothing -> Nothing
+      Just m  -> Just m
+
+comb ::  Maybe a -> (a -> Maybe b) -> Maybe b
+comb Nothing _ = Nothing
+comb (Just m) f = f m
+
+doSomething' m = comb (comb (comb (Just m) getS1) getS2) getS3
+
+doSomething'' m = (Just m) `comb` getS1 `comb` getS2 `comb` getS3
+
+doSomething'''''' m = (Just m) >>= getS1 >>= getS2 >>= getS3
+
+doSomething''' m = do
+  s1 <- getS1 m
+  s2 <- getS2 s1
+  s3 <- getS3 s2
+  return s3
+
+doSomething'''' m = do
+  s1 <- getS1 m
+  s2 <- getS2 s1
+  getS3 s2
+
+
+
+return' x = Just x  
+doSomething''''' m = do
+  s1 <- getS1 m
+  s2 <- getS2 s1
+  s3 <- getS3 s2
+  return' s3
